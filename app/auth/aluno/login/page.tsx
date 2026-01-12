c681b94837aab875e7f775bfb342cb87d0bb08bb
Jordan Medeiros <jordan.medeiros@sinesys.com.br>
2026-01-12 08:15:54 -0300

    feat(auth): add login and signup redirect pages for aluno and professor

---
+++ app/auth/aluno/login/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function AlunoLoginPage() {
+  redirect('/auth/login')
+}
---
+++ app/auth/professor/cadastro/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function ProfessorCadastroPage() {
+  redirect('/auth/sign-up')
+}
