ecc8572cf9d20eaba41d3afdb6b0a4ce1cc06efb
Jordan Medeiros <jordan.medeiros@sinesys.com.br>
2026-01-18 12:10:39 -0300

    feat: Add new layout components for main navigation, project lists, dynamic breadcrumbs, and role-based sidebars.

--- components/layout/app-sidebar.tsx
+++ components/layout/app-sidebar.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 "use client"
 
 import { SuperAdminSidebar } from "@/components/layout/superadmin-sidebar"
--- components/layout/impersonation-banner.tsx
+++ components/layout/impersonation-banner.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 "use client"
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
