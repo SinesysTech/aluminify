175aac5437a535f7eee8aa220387041625369209
Jordan Medeiros <jordan.medeiros@sinesys.com.br>
2026-01-18 18:42:38 -0300

    feat: Create professor dashboard page that redirects professors to their company's dashboard or displays an error.

--- app/(dashboard)/professor/flashcards/page.tsx
+++
@@ -1,8 +0,0 @@
-﻿import { requireUser } from '@/lib/auth'
-import FlashcardsAdminClient from './flashcards-admin-client'
-
-export default async function FlashcardsAdminPage() {
-  await requireUser({ allowedRoles: ['professor'] })
-  return <FlashcardsAdminClient />
-}
-
