48af62caef832e6a1106a17130603c3cb3e27c4d
Jordan Medeiros <jordan.medeiros@sinesys.com.br>
2026-01-18 19:10:24 -0300

    feat: Add professor materials redirect page that redirects based on company slug or displays an error.

---
+++ app/(dashboard)/professor/materiais/page.tsx
@@ -0,0 +1,12 @@
+import { redirect } from 'next/navigation'
+import { requireUser } from '@/lib/auth'
+
+export default async function ProfessorMateriaisRedirectPage() {
+    const user = await requireUser({ allowedRoles: ['professor'] })
+
+    if (user.empresaSlug) {
+        redirect(`/${user.empresaSlug}/professor/materiais`)
+    }
+
+    return <div>Erro: Professor sem empresa associada. Contate o suporte.</div>
+}
